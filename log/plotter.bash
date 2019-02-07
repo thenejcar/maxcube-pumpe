de7a630f782a37c2090c31ce2994fdad3c8d9e0f
Nejc Kisek <nejc@kisek.si>
2019-02-07 22:31:35 +0100

    added temperatures scp to the bash script

--- log/plotter.bash
+++ log/plotter.bash
@@ -8,3 +8,4 @@ REMOTE_LOCATION=miza:dump/maxcube
 python3 -c "from plotter import plot_and_reset; plot_and_reset()"
 
 scp valves.pdf "$REMOTE_LOCATION/maxcube-valves_$TODAY.pdf"
+scp temps.pdf  "$REMOTE_LOCATION/maxcube-temps_$TODAY.pdf"
