75edbb71892bb2db6b8379de5fe45638da8c570f
Nejc Kisek <nejc@kisek.si>
2019-03-06 19:11:14 +0100

    missing $

--- log/plotter.bash
+++ log/plotter.bash
@@ -5,7 +5,7 @@ cd /opt/maxcube-pumpe/log/
 TODAY=$(date '+%Y-%m-%d')
 REMOTE_LOCATION=miza:dump/maxcube
 
-python3 -c "from plotter import plot; plot('{TODAY}')"
+python3 -c "from plotter import plot; plot('${TODAY}')"
 
 scp valves.pdf "$REMOTE_LOCATION/maxcube-valves_$TODAY.pdf"
 scp temps.pdf  "$REMOTE_LOCATION/maxcube-temps_$TODAY.pdf"
